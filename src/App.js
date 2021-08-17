c0998287235dd88b29f9c173ad653d8fb76d18f0
Akash sahu <asahu7744@gmail.com>
2021-08-17 23:48:19 +0530

    setting up folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./css/index.css";
+import collegeList from "./assets/colleges.json";
+import img1 from "./assets/img/college_01.jpg";
+import img2 from "./assets/img/college_02.jpg";
 
 const App = () => {
-  return <div>hello world</div>;
+  console.log(collegeList);
+  return (
+    <div className="container">
+      <div className="collegeCard">
+        <img src={img1} className="" alt="college" />
+      </div>
+      <div className="collegeCard">
+        <img src={img2} className="" alt="college" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
